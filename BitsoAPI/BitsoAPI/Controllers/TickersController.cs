c6b584e48172a3a8c508217b9c4a11ba118b6dd0
Manuel Naranjo Contreras <manuelnaranjo@outlook.com>
2020-06-04 19:02:20 -0500

    Ticker api consumption test

---
+++ BitsoAPI/BitsoAPI/Controllers/TickersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BitsoAPI.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace BitsoAPI.Controllers
+{
+    public class TickersController : Controller
+    {
+        //private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Tickers
+        public ActionResult Index()
+        {
+            List<Ticker> currentTicker = getTicker();
+
+            return View(currentTicker);
+        }
+
+        List<Ticker> getTicker()
+        {
+            List<Ticker> tickerList = new List<Ticker>();
+
+            using (WebClient webClient = new WebClient())
+            {
+                ServicePointManager.Expect100Continue = true;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+                var json = webClient.DownloadString("https://api.bitso.com/v3/ticker/?book=btc_mxn");
+                var ticker = JsonConvert.DeserializeObject<Ticker>(json);
+
+                tickerList.Add(ticker);
+            }
+
+            return tickerList;
+        }
+    }
+}
---
+++ BitsoAPI/BitsoAPI/Models/BitsoModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BitsoAPI.Models
+{
+    public class Ticker
+    {
+        public int Id { get; set; }
+        public bool success { get; set; }
+        public Payload payload { get; set; }
+    }
+
+    public class Payload
+    {
+        public string high { get; set; }
+        public string last { get; set; }
+        public DateTime created_at { get; set; }
+        public string book { get; set; }
+        public string volume { get; set; }
+        public string vwap { get; set; }
+        public string low { get; set; }
+        public string ask { get; set; }
+        public string bid { get; set; }
+        public string change_24 { get; set; }
+    }
+}
